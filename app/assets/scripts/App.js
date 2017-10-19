c7dfa46e7403fffb792d0669a0b8d110dcff8d5a
SBeatyCode <svenbtb42@gmail.com>
2017-10-18 23:10:48 -0400

    Styled the header, added webpack, made webpack automatically refresh within gulp, used webpack to import jabascript files, added Babel, changed our example/learning js stuff into ECMA6 syntax and use Bable to keep it supported by the browser

---
+++ app/assets/scripts/App.js
@@ -0,0 +1,16 @@
+//var $ = require('jquery');
+//var Person = require('./modules/Person');
+import Person from './modules/Person';
+
+class Adult extends Person {
+    adultThing() {
+        console.log("I love to do adult things.");
+    }
+}
+
+var chris = new Person('Chris', 'Metroid');
+var stephen = new Adult('Stephen', 'purple');
+
+chris.greet();
+stephen.greet();
+stephen.adultThing();
