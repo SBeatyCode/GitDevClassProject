73900bcf7a7bf50e64d6053a4d7ed30f64366420
SBeatyCode <svenbtb42@gmail.com>
2017-10-19 01:40:39 -0400

    Finished the header. Added mobile menu, button-toggle, button transition, menu transition, it's very pretty

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,16 +1,3 @@
-var $ = require('jquery');
-//var Person = require('./modules/Person');
-import Person from './modules/Person';
+import MobileMenu from './modules/MobileMenu';
 
-class Adult extends Person {
-    adultThing() {
-        console.log("I love to do adult things.");
-    }
-}
-
-var chris = new Person('Chris', 'Metroid');
-var stephen = new Adult('Stephen', 'purple');
-
-chris.greet();
-stephen.greet();
-stephen.adultThing();
+var mobileMenu = new MobileMenu();
