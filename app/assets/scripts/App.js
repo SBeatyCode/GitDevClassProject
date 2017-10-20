8a58ba6bad6976a0bafd34c92318a20c77fec361
SBeatyCode <svenbtb42@gmail.com>
2017-10-20 16:58:35 -0400

    Merged with the new modal system

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,8 +2,10 @@ import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/Reveal-On-Scroll';
 import $ from 'jquery';
 import StickyHeader from './modules/Sticky-Header';
+import Modal from './modules/Modal';
 
 var mobileMenu = new MobileMenu();
 var featuresRevealOnScroll = new RevealOnScroll($(".feature-item"), "85%");
 var TestimonialsRevealOnScroll = new RevealOnScroll($(".testimonial"), "60%");
 var stickyHeader = new StickyHeader();
+var modal = new Modal();
