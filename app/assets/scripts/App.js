f4b7634e169873f3804792e1c6a8af6aecc4c561
SBeatyCode <svenbtb42@gmail.com>
2017-10-20 13:39:59 -0400

    Added the waypoint module, made a scroll down to reveal effect for the features and testimonial section

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,3 +1,7 @@
 import MobileMenu from './modules/MobileMenu';
+import RevealOnScroll from './modules/Reveal-On-Scroll';
+import $ from 'jquery';
 
 var mobileMenu = new MobileMenu();
+var featuresRevealOnScroll = new RevealOnScroll($(".feature-item"), "85%");
+var TestimonialsRevealOnScroll = new RevealOnScroll($(".testimonial"), "60%");
