ab50ddcb0451858ef291d9dadaa657ab32a300df
SBeatyCode <svenbtb42@gmail.com>
2017-10-20 15:35:07 -0400

    Adding scrolling feature, so that items would fade in as a user scrolls down, and the correct link in the navbar also lights up. Added jquery scrolling for scorlling effect. Uses Waypoints

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,7 +1,9 @@
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/Reveal-On-Scroll';
 import $ from 'jquery';
+import StickyHeader from './modules/Sticky-Header';
 
 var mobileMenu = new MobileMenu();
 var featuresRevealOnScroll = new RevealOnScroll($(".feature-item"), "85%");
 var TestimonialsRevealOnScroll = new RevealOnScroll($(".testimonial"), "60%");
+var stickyHeader = new StickyHeader();
---
+++ app/assets/scripts/modules/Sticky-Header.js
@@ -0,0 +1,69 @@
+import $ from 'jquery';
+import smoothScroll from 'jquery-smooth-scroll';
+import waypoints from '../../../../node_modules/waypoints/lib/noframework.waypoints';
+
+class StickyHeader {
+    constructor() {
+        this.siteHeader = $(".site-header");
+        this.headerTriggerElement = $(".large-hero__title");
+        this.createHeaderWaypoint();
+        
+        this.pageSections = $(".page-section");
+        this.headerLinks = $(".primary-nav a");
+        this.createPageSectionWaypoints();
+        this.addSmoothScrolling();
+    }
+    
+    addSmoothScrolling() {
+        this.headerLinks.smoothScroll();
+    }
+    
+    createHeaderWaypoint() {
+        var thisObject = this;
+        new Waypoint({
+//            uses [0] because the first element in a jquery array obj is the native DOM element, which is what Waypoint element property needs to work right
+            element: this.headerTriggerElement[0],
+            handler: function(direction) {
+                if(direction == "down") {
+                    thisObject.siteHeader.addClass("site-header--dark");
+                } else {
+                    thisObject.siteHeader.removeClass("site-header--dark");
+                }
+            }
+        });
+    }
+    
+    createPageSectionWaypoints() {
+        var thisObject = this;
+        this.pageSections.each(function() {
+            var currentPageSection = this;
+            //scrolling down
+            new Waypoint({
+                element: currentPageSection,
+                handler: function(direction) {
+                    if(direction == "down") {
+                        var matchingHeaderLink = currentPageSection.getAttribute("data-matching-link");
+                        thisObject.headerLinks.removeClass("is-current-link");
+                        $(matchingHeaderLink).addClass("is-current-link");
+                    }
+                },
+                offset: "18%"
+            });
+            
+            //scrolling up
+            new Waypoint({
+                element: currentPageSection,
+                handler: function(direction) {
+                    if(direction == "up") {
+                        var matchingHeaderLink = currentPageSection.getAttribute("data-matching-link");
+                        thisObject.headerLinks.removeClass("is-current-link");
+                        $(matchingHeaderLink).addClass("is-current-link");
+                    }
+                },
+                offset: "-40%"
+            });
+        });
+    }
+}
+
+export default StickyHeader;
